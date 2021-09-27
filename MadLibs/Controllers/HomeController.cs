66c2d982242cb3b3a4a687d1b2a7db2a31161736
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-27 11:38:17 -0700

    expand madlibs with more variables

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -10,12 +10,20 @@ namespace MadLibs.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/MadLibs")]
-    public ActionResult MadLibs(string holiday, string nounOne, string place) 
+    public ActionResult MadLibs(string holiday, string nounOne, string place, string person, string adjectiveOne, string bodyPart, string verb, string adjectiveTwo, string nounTwo, string food, string pluralNoun) 
     {
       MadLibsVariable myMadLibs = new MadLibsVariable();
       myMadLibs.Holiday = holiday;
       myMadLibs.NounOne = nounOne;
       myMadLibs.Place = place;
+      myMadLibs.Person = person;
+      myMadLibs.AdjectiveOne = adjectiveOne;
+      myMadLibs.BodyPart = bodyPart;
+      myMadLibs.Verb = verb;
+      myMadLibs.AdjectiveTwo = adjectiveTwo;
+      myMadLibs.NounTwo = nounTwo;
+      myMadLibs.Food = food;
+      myMadLibs.PluralNoun = pluralNoun;
       return View(myMadLibs); 
     }
   }
