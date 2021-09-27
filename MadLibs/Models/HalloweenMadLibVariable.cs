c2ad91e59feb6a0aab1526824a22f3e623c6c7db
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-27 15:35:08 -0700

    update names for expanded functionality

---
+++ MadLibs/Models/HalloweenMadLibVariable.cs
@@ -0,0 +1,17 @@
+namespace HalloweenMadLib.Models
+{
+  public class HalloweenMadLibVariable
+  {
+    public string Holiday { get; set; }
+    public string NounOne { get; set; }
+    public string Place { get; set; }
+    public string Person { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string BodyPart { get; set; }
+    public string Verb { get; set; }
+    public string AdjectiveTwo { get; set; }
+    public string NounTwo { get; set; }
+    public string Food { get; set; }
+    public string PluralNoun { get; set; }
+  }
+}
