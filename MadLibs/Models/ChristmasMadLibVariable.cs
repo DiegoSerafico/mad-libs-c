b276b5ecdf79ac0492548d0008919003100b6a26
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-27 15:36:22 -0700

    add christmas madlib form and variables

---
+++ MadLibs/Models/ChristmasMadLibVariable.cs
@@ -0,0 +1,15 @@
+namespace ChristmasMadLib.Models
+{
+  public class ChristmasMadLibVariable
+  {
+    public string NounOne { get; set; }
+    public string NounTwo { get; set; }
+    public string VerbOne { get; set; }
+    public string NounThree { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string NounFour { get; set; }
+    public string VerbTwo { get; set; }
+    public string NounFive { get; set; }
+    public string NounSix { get; set; }
+  }
+}
